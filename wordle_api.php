32e118fb3318b5c480814744596b9014834d70f8
ashleyr05 <zwu6su@virginia.edu>
2025-04-28 16:16:59 -0400

    editting wordle_api.php

--- wordle_api.php
+++ wordle_api.php
@@ -6,24 +6,11 @@ header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Au
 header("Access-Control-Max-Age: 1000");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 
-// Read input from the body of the request
-// (i.e., from STDIN) as JSON
-$json = file_get_contents("php://input");
-$input = json_decode($json, true);
-
-// Do processing of the data
-
-$output = [
-    "time" => date("Y-m-d g:i a"),
-    "request" => $input
-];
-
 //get words file
-$words = file_get_contents("https://cs4640.cs.virginia.edu/homework/angularwords.txt");
-$words_array = explode("\n", $words);
+$words = file("https://cs4640.cs.virginia.edu/homework/angularwords.txt", FILE_IGNORE_NEW_LINES);
 
 //get random word from word array
-$output["target word"] = array_rand($words_array);
+$output["target word"] = $words[array_rand($words)];
 
 // Output back to the client as JSON
 header("Content-Type: application/json");
